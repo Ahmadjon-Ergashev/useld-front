8b64f713b903dffa83c970bf9bb81382401b97e6
Ahmadjon-Ergashev <111.ahmadjon@gmail.com>
2024-02-29 02:21:23 +0500

    -PhoneInput

--- src/components/contact_form/ContactForm.jsx
+++ src/components/contact_form/ContactForm.jsx
@@ -61,7 +61,7 @@ export const ContactForm = () => {
                 </label>
 
                 <label className="block">
-                  <PhoneInput
+                  {/* <PhoneInput
                     country={'us'}
                     inputProps={{
                       name: 'phone',
@@ -70,7 +70,13 @@ export const ContactForm = () => {
                     }}
                     containerClass="w-full"
                     inputClass=" px-3 py-2 !border-[#082A58] !border shadow-sm  bg-[#FFF] border placeholder-[#082A58] focus:outline-none focus:border-sky-500 focus:ring-sky-500 block !w-full rounded-md sm:text-sm focus:ring-1"
-                  />
+                  /> */}
+                  <input
+                    type="number"
+                    name="phone"
+                    required
+                    className=" px-3 py-2 bg-[#FFF]   border shadow-sm border-[#082A58] placeholder-[#082A58] focus:outline-none focus:border-sky-500 focus:ring-sky-500 block w-full rounded-md sm:text-sm focus:ring-1"
+                    placeholder="+(998) --- -- -- " />
                 </label>
                 <label className="block ">
                   <input
